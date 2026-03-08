e28813e24ef46ef579c7ee23565a5a6d3120dcbb
Shivansh <132641847+shivnsshh@users.noreply.github.com>
2026-03-08 15:47:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function redlight(){
+    document.getElementById("red").style.background="red";
+    document.getElementById("yellow").style.background="gray";
+    document.getElementById("green").style.background="gray";
+    
+}
+
+function yellowlight(){
+    document.getElementById("red").style.background="gray";
+    document.getElementById("yellow").style.background="yellow";
+    document.getElementById("green").style.background="gray";
+    
+}
+function greenlight(){
+    document.getElementById("red").style.background="gray";
+    document.getElementById("yellow").style.background="gray";
+    document.getElementById("green").style.background="#26ff00";
+    
+}
